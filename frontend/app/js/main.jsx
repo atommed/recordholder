95d67a98186cae5780248e73d1b91060b3f6b9c0
Gregory <xalvevs@gmail.com>
2016-09-08 14:12:11 +0300

    Frontend gulp

--- frontend/app/js/main.jsx
+++ frontend/app/js/main.jsx
@@ -2,4 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 var mp = document.getElementById("app_mount")
-ReactDOM.render(<h1>Hello!</h1>, mp)
+ReactDOM.render(
+	<div>
+		<h1>Hello gulp!</h1>		
+		<h2>This is my website!</h2>
+		<h3>Lel</h3>
+	</div>,
+	mp)
