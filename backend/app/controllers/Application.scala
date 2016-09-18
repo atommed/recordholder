276a18a2521d357dca3dfeea93a21c58cc995c46
Gregory <xalvevs@gmail.com>
2016-09-18 15:01:49 +0300

    Add file upload

--- backend/app/controllers/Application.scala
+++ backend/app/controllers/Application.scala
@@ -9,6 +9,7 @@ import play.api.mvc._
 
 
 class Application @Inject() (implicit system: ActorSystem, materializer: Materializer) extends Controller {
+  private var i: Int = 0
   def index = Action {
     Ok("It works!")
   }
@@ -22,4 +23,11 @@ class Application @Inject() (implicit system: ActorSystem, materializer: Materia
   def socket = WebSocket.accept[String,String](req =>
     ActorFlow.actorRef(out => MyWebSocketActor.props(out))
   )
+
+  def fileUploadTest = Action(parse.multipartFormData) { request =>
+    i += 1
+    request.body.file("music").map(track =>
+      Ok(s"#${i} ${track.filename}")
+    ).getOrElse(BadRequest("Can't find track in form"))
+  }
 }
