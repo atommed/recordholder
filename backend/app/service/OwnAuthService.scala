dcf2e1f63cab89153bccb300a5fb42d09b3b38c9
Gregory K <xalvevs@gmail.com>
2016-12-17 00:42:03 +0200

    Saving

--- backend/app/service/OwnAuthService.scala
+++ backend/app/service/OwnAuthService.scala
@@ -12,49 +12,44 @@ import util.AuthUtils
 import util.DBUtils.Implicit.FuturedDb
 
 import scala.concurrent.Future
-import scala.util.Try
 
 @Singleton()
 class OwnAuthService @Inject()(db: Database) {
   private def isUserExists(login: String)
-                        (implicit c: Connection) =
+                        (implicit c: Connection) = {
     SQL"""
           SELECT 1 FROM own_auth WHERE login = $login
       """
       .as(SqlParser.scalar[Int].singleOpt).nonEmpty
+  }
 
   private def createUser(name: String)
-                        (implicit c: Connection): Long =
+                        (implicit c: Connection): Long = {
     SQL"""
           INSERT INTO app_user(name) VALUES($name)
        """
       .executeInsert(SqlParser.scalar[Long].single)
+  }
 
   private def addOwnUserAuth(userId: Long, login: String, hash: Array[Byte], salt: Array[Byte])
-                            (implicit c: Connection)=
+                            (implicit c: Connection) = {
     SQL"""
           INSERT INTO own_auth(user_id, login, password_hash, salt)
           VALUES($userId, $login, $hash, $salt)
        """
-    .executeInsert()
+      .executeInsert()
+  }
 
   private def findUserCredentials(login: String)
                                  (implicit c: Connection) = {
-    import SqlParser._
-    val parser = for{
-      a <- long("id")
-      b <- str("name")
-      c <- byteArray("password_hash")
-      d <- byteArray("salt")
-    } yield (a,b,c,d)
-    import  SqlParser.{scalar=>s, flatten}
+    import  SqlParser._
     SQL"""
           SELECT u.id, u.name, oa.password_hash, oa.salt
           FROM own_auth oa JOIN app_user u
           ON oa.user_id = u.id
           WHERE oa.login = $login
       """
-      .as(parser.singleOpt)
+      .as((long(1) ~ str(2) ~ byteArray(3) ~ byteArray(4) map flatten).singleOpt)
   }
 
   def register(credentials: Credentials): Future[User] = {
@@ -73,7 +68,7 @@ class OwnAuthService @Inject()(db: Database) {
 
   def signIn(credentials: Credentials): Future[Option[User]] = {
     val (login, password) = (credentials.login, credentials.password)
-    db.futureQuery(implicit c=>findUserCredentials(login)).map(_.flatMap({
+    db.futureQuery(findUserCredentials(login)(_)).map(_.flatMap({
       case (userId, userName, realHash, salt) =>
         val genSalt = AuthUtils.genHash(salt, password)
         if(genSalt sameElements realHash)
--- backend/app/service/TrackService.scala
+++ backend/app/service/TrackService.scala
@@ -1,20 +1,23 @@
 package service
 
 import java.io.File
-import java.nio.file.{Files, Paths}
+import java.nio.file.Paths
+import java.sql.Connection
 import javax.inject.{Inject, Singleton}
 
-import models.{Album, Track, User}
+import anorm.SqlParser.scalar
+import anorm._
+import models.{Album, Artist, Track, User}
 import play.api.Configuration
-
-import scala.collection.JavaConverters.mapAsScalaMapConverter
+import play.api.db.Database
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import util.DBUtils.Implicit.FuturedDb
 import util.MetadataRetriever
 
-import scala.util.{Failure, Success}
-
+import scala.collection.JavaConverters.mapAsScalaMapConverter
 
 @Singleton
-class TrackService @Inject()(conf: Configuration) {
+class TrackService @Inject()(db: Database, conf: Configuration) {
   private val storage = Paths.get(conf.getString("storage").get)
   private val tracksPath = storage.resolve("tracks")
   private val coversPath = storage.resolve("covers")
@@ -35,13 +38,110 @@ class TrackService @Inject()(conf: Configuration) {
     }
   }
 
-  def persistTrack(track: File, originalName: String, uploader: User) = {
-    val metadata = analyzer.get().extractMetadata(track)
+  def addTrack(u: User, t: Track)(implicit c: Connection): Long = {
+    val newTrackId =
+      SQL"""
+          INSERT INTO track
+          (title, original_name, extension, length, bitrate, has_cover, artist_id, album_id)
+          VALUES
+          (${t.title}, ${t.originalName}, ${t.extension}, ${t.length}, ${t.bitrate}, ${t.hasCover}, ${t.artistId}, ${t.albumId})
+       """.executeInsert(scalar[Long].single)
+    SQL"INSERT INTO user_tracks(user_id, track_id) VALUES(${u.id}, ${t.id})".execute()
+    newTrackId
+  }
+
+  def findAlbum(u: User, name: String)(implicit c: Connection) = {
+    val parser: RowParser[Album] = Macro.namedParser[Album]
+    SQL"""
+         SELECT id, artist_id, name, description
+         FROM user_albums JOIN album ON user_albums.user_id = album.id
+         WHERE user_albums.user_id = ${u.id} AND album.name = $name
+         ORDER BY album.artist_id NULLS FIRST
+         LIMIT 1
+       """.as(parser.singleOpt)
+  }
+
+  def findArtist(u: User, name: String)(implicit c: Connection) = {
+    val parser: RowParser[Artist] = Macro.namedParser[Artist]
+    SQL"""
+         SELECT id, name, description
+         FROM user_artists JOIN artist ON user_artists.user_id = artist.id
+         WHERE user_artists.user_id = ${u.id} AND artist.name = $name
+         LIMIT 1
+       """.as(parser.singleOpt)
+  }
+
+  def addArtist(u: User, name: String)(implicit c: Connection) = {
+    val artistId=
+      SQL"""
+            INSERT INTO artist(name) VALUES($name)
+         """
+      .executeInsert(scalar[Long].single)
+    SQL"""
+         INSERT INTO user_artists(user_id, artist_id) VALUES (${u.id}, $artistId)
+       """.executeInsert()
+    artistId
+  }
+
+  def addAlbum(u: User, name: String)(implicit c: Connection) = {
+    val albumId =
+      SQL"""
+           INSERT INTO album(name) VALUES($name)
+         """
+        .executeInsert(scalar[Long].single)
+    SQL"""
+         INSERT INTO user_albums(user_id, album_id) VALUES(${u.id}, $albumId)
+       """
+      .executeInsert()
+    albumId
+  }
+
+
+  def persistTrack(file: File, originalName: String, uploader: User) = {
+    val metadata = analyzer.get().extractMetadata(file)
     val tags = metadata.getMetadata.asScala.map({case (key, value) => (key.trim.toLowerCase, value)})
     val extension = getExtension(metadata.getPossibleExtensions)
+    val title = tags.get("title")
+    val length = metadata.getLength
+    val bitrate = metadata.getBitrate
+    val hasCover = metadata.hasCover
 
-    (tags.get("album"), tags.get("artist")) match {
+    val track = Track(
+      title = tags.getOrElse("title", originalName),
+      originalName = originalName,
+      extension = getExtension(metadata.getPossibleExtensions),
+      length = metadata.getLength,
+      bitrate = metadata.getBitrate,
+      hasCover = metadata.hasCover
+    )
+
+    val q = (tags.get("album"), tags.get("artist")) match {
       case (None, None) =>
+        db.futureQuery(addTrack(uploader,track)(_))
+
+      case (Some(album), None) =>
+        db.futureQuery(findAlbum(uploader,album)(_)).flatMap({
+          case Some(album)=>
+            db.futureQuery(addTrack(uploader,track.copy(albumId = Some(album.id), artistId = album.artistId))(_))
+          case None =>
+            for{
+              albumId <- db.futureQuery(addAlbum(uploader, album)(_))
+              trackId <- db.futureQuery(addTrack(uploader, track)(_))
+            } yield trackId
+        })
+
+      case (None, Some(artist)) =>
+        db.futureQuery(findArtist(uploader, artist)(_)).flatMap({
+          case Some(artist)=>
+            db.futureQuery(addTrack(uploader, track.copy(artistId = Some(artist.id)))(_))
+          case None=>
+            for{
+              artistId <- db.futureQuery(addAlbum(uploader, artist)(_))
+              trackId <- db.futureQuery(addTrack(uploader, track.copy(artistId = Some(artistId)))(_))
+            } yield trackId
+        })
+
+      case _ => scala.concurrent.Future.successful(42l)
     }
   }
   /*
